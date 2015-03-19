8bd2f4421e6a06fd01591fa36c17ba32b1a5c067
Sedat Ak <sedat.ak.bm@gmail.com>
2015-03-19 15:22:43 +0200

    anasayfa eklendi

---
+++ lazimlik/lazimlik/views.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from django.http import *
+from django.template import Template, Context
+from django.shortcuts import render_to_response, redirect, render
+
+def anasayfa(request):
+    return render_to_response('anasayfa.html')
