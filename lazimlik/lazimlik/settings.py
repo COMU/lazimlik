7da93db2a037ccd83fad9e26d13ea347f616b19f
Sedat Ak <sedat.ak.bm@gmail.com>
2015-03-25 22:35:30 +0200

    dil Türkçe'ye çevrildi

--- lazimlik/lazimlik/settings.py
+++ lazimlik/lazimlik/settings.py
@@ -64,13 +64,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        }
 }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'tr-TR'
 
 TIME_ZONE = 'UTC'
 
