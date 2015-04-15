5822edce1c41ebc71dd35f138df16119e580cb9f
Sedat Ak <sedat.ak.bm@gmail.com>
2015-04-15 20:55:56 +0300

    twitter bootstrap eklendi

--- lazimlik/lazimlik/settings.py
+++ lazimlik/lazimlik/settings.py
@@ -81,8 +81,19 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+        )
+
+# List of finder classes that know how to find static files in various locations.
+STATICFILES_FINDERS = (
+        'django.contrib.staticfiles.finders.FileSystemFinder',
+        'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+        #'django.contrib.staticfiles.finders.DefaultStorageFinder',
+        )
