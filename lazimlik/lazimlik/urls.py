8d4defcfd4a0c1df8ece90bad22bb07d0e274f6c
Sedat Ak <sedat.ak.bm@gmail.com>
2015-05-14 15:18:13 +0300

    login durumuna göre sayfalar ayarlandı

--- lazimlik/lazimlik/urls.py
+++ lazimlik/lazimlik/urls.py
@@ -6,20 +6,23 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'lazimlik.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+	# Examples:
+	# url(r'^$', 'lazimlik.views.home', name='home'),
+	# url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'yonetim.views.anasayfa', name='anasayfa'),
-    url(r'^about/$', 'yonetim.views.about', name='about'),
-    url(r'^home/$', 'social_app.views.home', name='home'),
-    url(r'^user/$', 'yonetim.views.user', name='user'),
-    url(r'^isverelim/$', 'yonetim.views.isverelim', name='isverelim'),
-    url(r'^isyapalim/$', 'yonetim.views.isyapalim', name='isyapalim'),
-    url(r'', include('social_auth.urls')),
-    url(r'^login/$', 'social_app.views.login', name='login'),
-    url(r'^logout/$', 'social_app.views.logout', name='logout'),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', 'yonetim.views.anasayfa', name='anasayfa'),
+	url(r'^about/$', 'yonetim.views.about', name='about'),
+	url(r'^about_user/$', 'yonetim.views.about_user', name='about_user'),
+	url(r'^home/$', 'social_app.views.home', name='home'),
+	url(r'^user/$', 'yonetim.views.user', name='user'),
+	url(r'^isverelim/$', 'yonetim.views.isverelim', name='isverelim'),
+	url(r'^isyapalim/$', 'yonetim.views.isyapalim', name='isyapalim'),
+	url(r'^isyapalim_user/$', 'yonetim.views.isyapalim_user', name='isyapalim_user'),
+	url(r'', include('social_auth.urls')),
+	url(r'^login/$', 'social_app.views.login', name='login'),
+	url(r'^logout/$', 'social_app.views.logout', name='logout'),
+	url(r'^is_al/$', 'yonetim.views.is_al', name='is_al'),
 )
 
 if settings.DEBUG:
--- lazimlik/yonetim/views.py
+++ lazimlik/yonetim/views.py
@@ -19,6 +19,9 @@ def anasayfa(request):
 def about(request):
 	return render_to_response("about.html")
 
+def about_user(request):
+	return render_to_response("about_user.html")
+
 @login_required
 def isverelim(request):
 
@@ -34,20 +37,30 @@ def isverelim(request):
 
 @login_required
 def user(request):
-	form = UserProfileForm(request.POST or None)
-	form.fields['user'].widget = forms.HiddenInput()
-
-	if request.POST:
-		form = UserProfileForm({'user':request.user.id})
-		initial_value = {'user':request.user.id,'bitirilen_is_puani':0,'teslim_edilmeyen_is':0,'yaptirilan_is_puani':0,'teslim_alinmayan_is':0,'rumuz':request.POST['rumuz']}
-		form = UserProfileForm(initial_value)
-		print form
-		form.save()
-		messages.success(request, 'Rumuz eklendi.')
+	if UserProfile.objects.get(user=request.user):
 		return HttpResponseRedirect('/home')
+	else:
+		form = UserProfileForm(request.POST or None)
+		form.fields['user'].widget = forms.HiddenInput()
+
+		if request.POST:
+			form = UserProfileForm({'user':request.user.id})
+			initial_value = {'user':request.user.id,'bitirilen_is_puani':0,'teslim_edilmeyen_is':0,'yaptirilan_is_puani':0,'teslim_alinmayan_is':0,'rumuz':request.POST['rumuz']}
+			form = UserProfileForm(initial_value)
+			print form
+			form.save()
+			messages.success(request, 'Rumuz eklendi.')
+			return HttpResponseRedirect('/home')
 
 	return render_to_response("user.html", locals(), context_instance=RequestContext(request))
 
 def isyapalim(request):
 	isler = YapilacakIs.objects.all()
 	return render_to_response("isyapalim.html",locals(), context_instance=RequestContext(request))
+
+def isyapalim_user(request):
+	isler = YapilacakIs.objects.all()
+	return render_to_response("isyapalim_user.html",locals(), context_instance=RequestContext(request))
+
+def is_al(request):
+	return render_to_response("userdetail.html")
